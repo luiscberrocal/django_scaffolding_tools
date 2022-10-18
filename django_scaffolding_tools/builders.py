2d892b2060af66d4f4d5c2af489232662be6d397
luiscberrocal <luis.berrocal.1942@gmail.com>
2022-10-18 07:45:57 -0500

    WIP

---
+++ django_scaffolding_tools/builders.py
@@ -0,0 +1,21 @@
+from typing import Any, List, Dict
+
+
+def build_serializer_template_data(model_list: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
+    """Builds a dictionary to be used in the Jinja template"""
+    template_models = list()
+    for model in model_list:
+        serializer_data = dict()
+        serializer_data['name'] = model['name']
+        serializer_data['attributes'] = list()
+        for attribute in serializer_data['attributes']:
+            serializer_attribute = dict()
+            serializer_attribute['name'] = attribute['name']
+            keywords = list()
+            for keyword in attribute['serializer'].get('keywords'):
+                keywords.append(f'{keyword["name"]}={keyword["value"]}')
+            field_vars = ', '.join(keyword)
+            serializer_data['field'] = f'serializers.{attribute["serializer"]["field_type"]}({field_vars})}'
+
+        template_models.append(serializer_data)
+    return template_models
