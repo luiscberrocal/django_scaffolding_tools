4c041d61a60908a03bc4cc56e152b8652a93a223
luiscberrocal <luis.berrocal.1942@gmail.com>
2022-10-20 07:50:18 -0500

    Added CSV Writer.

--- tests/test_writer.py
+++ tests/test_writer.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
-from django_scaffolding_tools.writers import write_serializer_from_file
+from django_scaffolding_tools.parsers import parse_file_for_ast_classes, parse_for_django_classes
+from django_scaffolding_tools.writers import write_serializer_from_file, write_django_model_csv
 
 
 def test_write_serializers(output_folder):
@@ -10,3 +11,14 @@ def test_write_serializers(output_folder):
     target_file = output_folder / 'test_write_serializers.py'
     write_serializer_from_file(source_filename, target_file)
     assert target_file.exists()
+
+
+def test_write_django_model_csv(output_folder, fixtures_folder):
+    csv_filename = output_folder / 'model.csv'
+    module_file = 'models_with_helptext.py'
+    filename = fixtures_folder / module_file
+
+    ast_module = parse_file_for_ast_classes(filename)
+    django_classes = parse_for_django_classes(ast_module)
+    write_django_model_csv(django_classes['classes'], csv_filename)
+
