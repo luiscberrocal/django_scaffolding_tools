6f0911bfbcea364bbe790f6edee86072103a6dd6
luiscberrocal <luis.berrocal.1942@gmail.com>
2022-10-28 08:17:04 -0400

    Added AssertionTuples

--- tests/utils/test_assert_utils.py
+++ tests/utils/test_assert_utils.py
@@ -4,9 +4,9 @@ from django_scaffolding_tools.utils.assert_utils import generate_dict_assertions
 
 
 def test_generate_assert_dict():
-    data_dict = {'name': 'Bruce', 'date_of_birth': datetime, 'pets': [{'type': 'dog'}],
-                 'address': {'street': 'First street', 'zip_code': '999999'}}
+    data_dict = {'name': 'Bruce', 'date_of_birth': datetime(1966, 5, 5, 13, 15, 0), 'pets': [{'type': 'dog'}],
+                 'address': {'street': 'First street', 'zip_code': '999999', 'super_hero': True, 'power': 7}}
     # data_dict = {'name': 'Bruce'}
     assert_list = generate_dict_assertions(data_dict, 'data_dict')
-    print(assert_list)
-
+    for assertion_tuple in assert_list:
+        print(f'{assertion_tuple.var_name} == {assertion_tuple.value}')
