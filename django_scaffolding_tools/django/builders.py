53ed15578766cdc0fd1f37ad608435af68412c43
luiscberrocal <luis.berrocal.1942@gmail.com>
2022-11-01 08:19:05 -0400

    Serializer build working with camel case.

--- django_scaffolding_tools/django/builders.py
+++ django_scaffolding_tools/django/builders.py
@@ -3,7 +3,8 @@ from typing import Any, Dict, List
 import humps
 
 
-def build_model_serializer_template_data(parsed_django_classes: Dict[str, Any], add_source_camel_case=False) -> List[Dict[str, Any]]:
+def build_model_serializer_template_data(parsed_django_classes: Dict[str, Any],
+                                         add_source_camel_case=False) -> List[Dict[str, Any]]:
     template_data = list()
 
     for model in parsed_django_classes['classes']:
@@ -14,13 +15,19 @@ def build_model_serializer_template_data(parsed_django_classes: Dict[str, Any], 
         for attribute in model['attributes']:
             serializer_attribute = dict()
             serializer_attribute['name'] = attribute['name']
+            keywords = dict()
             if add_source_camel_case:
                 source = humps.camelize(attribute['name'])
                 if source != attribute['name']:
                     serializer_attribute['source'] = source
-                    serializer_attribute['serializer'] = f'serializers.{attribute["data_type"]}(source=\'{source}\')'
-                #if attribute['data_type'] == 'ForeignKey':
-                #    serializer_attribute['serializer'] = f'{attribute[]}'
+                    keywords['source'] = source
+                keyword_content = ''
+                for keyword, value in keywords.items():
+                    keyword_content += f'{keyword}=\'{value}\', '
+                serializer_attribute['serializer'] = f'serializers.{attribute["data_type"]}({keyword_content[:-2]})'
+                if attribute['data_type'] == 'ForeignKey':
+                    classname = attribute["arguments"][0]["value"]
+                    serializer_attribute['serializer'] = f'{classname}Serializer({keyword_content[:-2]})'
             serializer_data['fields'].append(serializer_attribute)
         template_data.append(serializer_data)
 
--- tests/django/test_django_builders.py
+++ tests/django/test_django_builders.py
@@ -3,6 +3,7 @@ import pytest
 from django_scaffolding_tools.django.builders import build_model_serializer_template_data
 from django_scaffolding_tools.django.parsers import parse_for_django_classes
 from django_scaffolding_tools.parsers import parse_file_for_ast_classes
+from django_scaffolding_tools.utils.assert_utils import generate_pytest_assertions
 from tests.test_parsers import quick_write
 
 
@@ -11,10 +12,31 @@ def test_build_model_serializer_template_data_camelize(output_folder, fixtures_f
     filename = fixtures_folder / model_filename
     # 1 Convert model.py to an ast json file.
     ast_dict = parse_file_for_ast_classes(filename)
-    quick_write(ast_dict, f'ast_{model_filename}.json', output_subfolder='django')
     # 2 Parse AST json dictionary for Django Model data
     model_data = parse_for_django_classes(ast_dict)
-    quick_write(model_data, f'model_data_{model_filename}.json', output_subfolder='django')
     # 3 Build serializer data form Django model data
     serializer_data = build_model_serializer_template_data(model_data, add_source_camel_case=True)
-    quick_write(serializer_data, f'serializer_data_{model_filename}.json', output_subfolder='django')
+    # quick_write(ast_dict, f'ast_{model_filename}.json', output_subfolder='django')
+    # quick_write(model_data, f'model_data_{model_filename}.json', output_subfolder='django')
+    # quick_write(serializer_data, f'serializer_data_{model_filename}.json', output_subfolder='django')
+
+    assert len(serializer_data) == 2
+
+    model1 = serializer_data[0]
+    assert model1['model'] == 'Clinic'
+    assert model1['fields'][0]['name'] == 'name'
+    assert model1['fields'][0]['serializer'] == 'serializers.CharField()'
+
+    model2 = serializer_data[1]
+    assert model2['model'] == 'Patient'
+    assert model2['fields'][0]['name'] == 'first_name'
+    assert model2['fields'][0]['source'] == 'firstName'
+    assert model2['fields'][0]['serializer'] == 'serializers.CharField(source=\'firstName\')'
+    assert model2['fields'][1]['name'] == 'last_name'
+    assert model2['fields'][1]['source'] == 'lastName'
+    assert model2['fields'][1]['serializer'] == 'serializers.CharField(source=\'lastName\')'
+    assert model2['fields'][2]['name'] == 'owning_clinic'
+    assert model2['fields'][2]['source'] == 'owningClinic'
+    assert model2['fields'][2]['serializer'] == 'ClinicSerializer(source=\'owningClinic\')'
+
+
