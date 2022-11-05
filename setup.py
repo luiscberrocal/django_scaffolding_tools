d8b56feb51a6be26892b0150739fcbc5a382ec4f
luiscberrocal <luis.berrocal.1942@gmail.com>
2022-11-05 11:06:11 -0400

    Bump to version 0.9.0

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/luiscberrocal/django_scaffolding_tools',
-    version='0.8.1',
+    version='0.9.0',
     zip_safe=False,
     package_data={'': ['templates/*.j2']},
 )
