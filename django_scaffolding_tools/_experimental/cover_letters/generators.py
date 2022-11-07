e82ca4703bd5f3e2d872a04a9aa531dd8e47c344
luiscberrocal <luis.berrocal.1942@gmail.com>
2022-11-07 10:31:33 -0500

    Write to pdf

--- django_scaffolding_tools/_experimental/cover_letters/generators.py
+++ django_scaffolding_tools/_experimental/cover_letters/generators.py
@@ -1,10 +1,20 @@
+from datetime import datetime
 from pathlib import Path
 from typing import Dict, Any
 
+from docx2pdf import convert
 from docxtpl import DocxTemplate
 
+from django_scaffolding_tools._experimental.cover_letters.utils import run_commands
 
-def write_cover_letter(template_file: Path, context: Dict[str, Any], output_file:Path):
+
+def convert_docx_to_pdf(docx_file: Path, output_folder: Path):
+    command = ['libreoffice', '--headless', '--convert-to', 'pdf', '--outdir', f'{output_folder}',
+               f'{docx_file}']
+    result, errors = run_commands(command)
+
+
+def write_docx_cover_letter(template_file: Path, context: Dict[str, Any], output_file: Path):
     # Open our master template
     doc = DocxTemplate(template_file)
     # Load them up
@@ -12,3 +22,17 @@ def write_cover_letter(template_file: Path, context: Dict[str, Any], output_file
     # Save the file with personalized filename
     doc.save(output_file)
 
+
+if __name__ == '__main__':
+    root_folder = Path(__file__).parent.parent.parent.parent
+    fixtures_folder = root_folder / 'tests' / 'fixtures'
+    out_folder = root_folder / 'output'
+
+    template = fixtures_folder / '_experimental' / 'Cover Letter Template.docx'
+    today = datetime.today()
+    context = {'today': today.strftime('%b %M %Y'), 'position_name': 'Jedi', 'company_name': 'Jedi Order'}
+
+    cover_letter = out_folder / f'{today.strftime("%Y%m%d")}_cover_{context["company_name"]}_{context["position_name"]}.pdf'
+    cover_letter.unlink(missing_ok=True)
+
+    write_docx_cover_letter(template, context, cover_letter)
