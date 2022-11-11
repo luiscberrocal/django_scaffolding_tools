2af28d55381481726242c486d214ecd4d9698d70
luiscberrocal <luis.berrocal.1942@gmail.com>
2022-11-11 06:11:03 -0500

    Clean docker postgress

--- django_scaffolding_tools/_experimental/docker/clean_postgres.py
+++ django_scaffolding_tools/_experimental/docker/clean_postgres.py
@@ -17,12 +17,6 @@ def run_commands(commands: List[str], encoding: str = 'utf-8') -> Tuple[List[str
     return result_lines, error_lines
 
 
-def run_command2(cmd):
-    # cmd = "ps -A|grep 'process_name'"
-    ps = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    output = ps.communicate()[0]
-    return output, ['err']
-
 
 def get_containers(regexp):
     ps = subprocess.run(['docker', 'ps', '-a'], check=True, capture_output=True)
@@ -31,19 +25,63 @@ def get_containers(regexp):
                                 input=ps.stdout, capture_output=True)
     results = containers.stdout.decode('utf-8').strip().split('\n')
     container_list = list()
-    for r in results:
-        data = r.split(' ')
-        container_list.append({'container_id': data[0], 'image': data[3], 'name': data[-1]})
+    if len(results) > 0:
+        for r in results:
+            data = r.split(' ')
+            if len(data[0]) != 0:
+                container_list.append({'container_id': data[0], 'image': data[3], 'name': data[-1]})
     return container_list
 
 
-if __name__ == '__main__':
-    regexp = 'lms_[a-z_]*_postgres'
+def get_volumes(regexp):
+    ps = subprocess.run(['docker', 'volume', 'ls'], check=True, capture_output=True)
+    # print(ps.stderr.decode('utf-8').strip())
+    volumes = subprocess.run(['grep', '-E', f'{regexp}'],
+                             input=ps.stdout, capture_output=True)
+    results = volumes.stdout.decode('utf-8').strip().split('\n')
+    volume_list = list()
+    for r in results:
+        data = r.split(' ')
+        volume_list.append({'driver': data[0], 'name': data[-1]})
+    return volume_list
 
-    res = get_containers(regexp)
-    for i, r in enumerate(res, ):
-        print(f'{i} {r["container_id"]} {r["image"]}')
-    container_to_delete = input(f'Type the number of the container to delete (#):')
-    container_id = int(container_to_delete)
-    print(res[container_id])
+
+if __name__ == '__main__':
+    regexpression = r'lms-graph[a-z_\-]*_postgres'
+
+    res = get_containers(regexpression)
+    if len(res) != 0:
+        for i, r in enumerate(res):
+            print(f'{i} {r["container_id"]} {r["image"]}')
+        container_to_delete = input(f'Type the number of the container to delete (#):')
+        container_id = int(container_to_delete)
+        # print(res[container_id])
+
+        delete_container_command = ['docker', 'rm', res[container_id]['container_id']]
+        dc_res, errors = run_commands(delete_container_command)
+        print(f'Deleted container {dc_res}')
+    else:
+        print(f'No container found for {regexpression}')
+
+    d_vols = get_volumes(regexpression)
+    # print(d_vols)
+    if len(d_vols) != 0:
+        for i, d_vol in enumerate(d_vols):
+            print(f'({i}) {d_vol["name"]}')
+
+        volume_to_delete = input('Volume to delete (#, n, a):')
+        delete_volume_command = ['docker', 'volume', 'rm']
+        if volume_to_delete.lower() == 'a':
+            for d_vol in d_vols:
+                delete_volume_command.append(d_vol['name'])
+        elif isinstance(volume_to_delete, int):
+            delete_volume_command.append(d_vols[volume_to_delete]['name'])
+        else:
+            print('Not deleting any volumes')
+        if len(delete_volume_command) > 3:
+            v_res,v_errors = run_commands(delete_volume_command)
+            print(f'Deleted {v_res}')
+    else:
+        print(f'No volumes found for {regexpression}')
+
 
