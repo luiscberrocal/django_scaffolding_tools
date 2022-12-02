81f0b07b2dcaedcae2a1ff3f5b78a6d113a86fd9
luiscberrocal <luis.berrocal.1942@gmail.com>
2022-12-01 20:49:59 -0500

    Filename clenup.

--- django_scaffolding_tools/_experimental/general_ledger/cli.py
+++ django_scaffolding_tools/_experimental/general_ledger/cli.py
@@ -1,9 +1,10 @@
+import logging
 import shutil
 from datetime import datetime
 from pathlib import Path
 
-from .parsers import parse_general_ledger
-from .writers import write_transactions
+from django_scaffolding_tools._experimental.general_ledger.parsers import parse_general_ledger
+from django_scaffolding_tools._experimental.general_ledger.writers import write_transactions
 
 
 def separate_accounts(gl_file: Path, output_folder: Path, timestamp_format: str = '%Y%m%d_%H%M%S') -> Path:
@@ -15,16 +16,29 @@ def separate_accounts(gl_file: Path, output_folder: Path, timestamp_format: str 
     return output_file
 
 
+def clean_filename(filename: str):
+    new_name = filename.replace('.', '').replace(',', '').replace(' ', '_')
+    return new_name
+
+
 def build_output_file(gl_file: Path, output_folder: Path, timestamp_format: str = '%Y%m%d_%H%M%S') -> Path:
     """Creates a filename based on the gl_file values in the output folder prepending a timestamp."""
     timestamp = datetime.now().strftime(timestamp_format)
-    base_name = gl_file.name.replace(' ', '_')
-    output_file = output_folder / f'{timestamp}_{base_name}'
+    base_name = clean_filename(gl_file.stem)
+    extention = gl_file.suffix
+    output_file = output_folder / f'{timestamp}_{base_name}{extention}'
     return output_file
 
 
 if __name__ == '__main__':
     outputfolder = Path(__file__).parent.parent.parent.parent / 'output'
-    glfile = outputfolder / 'ITA GL Export 2022 (1).xlsx'
-    rs = separate_accounts(glfile, outputfolder)
-    print(rs)
+    # glfile = outputfolder / 'ITA GL Export 2022 (1).xlsx'
+    # rs = separate_accounts(glfile, outputfolder)
+    # print(rs)
+
+    log_file = outputfolder / 'exampel.log'
+    logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.DEBUG)
+    logging.debug('This message should go to the log file')
+    logging.info('So should this')
+    logging.warning('And this, too')
+    logging.error('And non-ASCII stuff, too, like Øresund and Malmö')
--- tests/_experimental/general_ledger/test_parsers.py
+++ tests/_experimental/general_ledger/test_parsers.py
@@ -1,7 +1,18 @@
 from django_scaffolding_tools._experimental.general_ledger.parsers import parse_general_ledger
 
+import logging
+
 
 def test_parse_general_ledger(output_folder):
     gl_file = output_folder / 'ITA GL Export 2022 (1).xlsx'
     trasactions = parse_general_ledger(gl_file)
     print(trasactions)
+
+
+def test_logging(output_folder):
+    log_file = output_folder / 'exampel.log'
+    logging.basicConfig(filename=log_file.root, encoding='utf-8', level=logging.DEBUG)
+    logging.debug('This message should go to the log file')
+    logging.info('So should this')
+    logging.warning('And this, too')
+    logging.error('And non-ASCII stuff, too, like Øresund and Malmö')
