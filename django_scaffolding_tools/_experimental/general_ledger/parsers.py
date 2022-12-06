265588cfe919b77477a64437ba09728ab737db63
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2022-12-06 16:03:17 -0500

    Added protocol.

--- django_scaffolding_tools/_experimental/general_ledger/parsers.py
+++ django_scaffolding_tools/_experimental/general_ledger/parsers.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import List, Any, Dict
+from typing import List, Any, Dict, Protocol
 
 from openpyxl.reader.excel import load_workbook
 from openpyxl.utils import get_column_letter
@@ -18,20 +18,35 @@ COLUMN_MAPPINGS = {
 }
 
 COLUMN_MAPPINGS_2 = {
-   '1': {'name': 'account_id', 'title': 'Account ID', 'width': 12},
-   '2': {'name': 'account_description', 'title': 'Account Description', 'width': 24},
-   '3': {'name': 'date', 'title': 'Date', 'number_format': 'DD/MM/YYYY', 'width': 12},
-   '4': {'name': 'reference', 'title': 'Reference', 'width': 30},
-   '5': {'name': 'journal', 'title': 'Jrnl', 'width': 12},
-   '6': {'name': 'description', 'title': 'Trans Description', 'width': 36},
-   '7': {'name': 'debit_amount', 'title': 'Debit Amt', 'number_format': '#,##0.00', 'width': 12},
-   '8': {'name': 'credit_amount', 'title': 'Credit Amt', 'number_format': '#,##0.00', 'width': 12},
-   '9': {'name': 'balance', 'title': 'Balance', 'number_format': '#,##0.00', 'width': 12},
+    '1': {'name': 'account_id', 'title': 'Account ID', 'width': 12},
+    '2': {'name': 'account_description', 'title': 'Account Description', 'width': 24},
+    '3': {'name': 'date', 'title': 'Date', 'number_format': 'DD/MM/YYYY', 'width': 12},
+    '4': {'name': 'reference', 'title': 'Reference', 'width': 30},
+    '5': {'name': 'journal', 'title': 'Jrnl', 'width': 12},
+    '6': {'name': 'description', 'title': 'Trans Description', 'width': 36},
+    '7': {'name': 'debit_amount', 'title': 'Debit Amt', 'number_format': '#,##0.00', 'width': 12},
+    '8': {'name': 'credit_amount', 'title': 'Credit Amt', 'number_format': '#,##0.00', 'width': 12},
+    '9': {'name': 'balance', 'title': 'Balance', 'number_format': '#,##0.00', 'width': 12},
 }
+
 
 def get_default_mappings():
     return COLUMN_MAPPINGS
 
+
+class ParsingConfiguration(Protocol):
+    @property
+    def start_row(self) -> int:
+        ...
+
+    @property
+    def sheet_name(self) -> str:
+        ...
+
+    @property
+    def column_mappings(self) -> Dict[str, Dict[str, Any]]:
+        ...
+    
 
 def parse_general_ledger(general_ledger_file: Path, start_row: int = 6,
                          sheet_name: str = 'General Ledger',
