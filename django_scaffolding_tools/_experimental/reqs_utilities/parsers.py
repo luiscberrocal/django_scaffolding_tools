7c4d088f65af38ac43e7fc88ce9b56ddd0c79ac4
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2022-12-28 15:11:32 -0500

    Parsing reqs for update

---
+++ django_scaffolding_tools/_experimental/reqs_utilities/parsers.py
@@ -0,0 +1,59 @@
+import json
+import re
+from pathlib import Path
+
+
+def parse_for_permitted_libs(req_file: Path):
+    regexp = re.compile(r'(?P<lib_name>[\w_\-]+)==(?P<version>[\w\.\-]+)\s*#?(?P<comment>.*)')
+    with open(req_file, 'r') as r_file:
+        lines = r_file.readlines()
+    parsed_requirements = dict()
+    for i, line in enumerate(lines, 1):
+        match = regexp.match(line)
+        if match:
+            lib_name = match.group('lib_name')
+            parsed_requirements[lib_name] = match.group('version')
+    return parsed_requirements
+
+
+def parse_requirement_file(req_file: Path):
+    regexp = re.compile(r'(?P<lib_name>[\w_\-]+)==(?P<version>[\w\.\-]+)\s*#?(?P<comment>.*)')
+    with open(req_file, 'r') as r_file:
+        lines = r_file.readlines()
+    var_names = ['lib_name', 'version', 'comment']
+    parsed_requirements = list()
+    for i, line in enumerate(lines, 1):
+        match = regexp.match(line)
+        req = {'raw': line, 'line_number': i, 'parsed': None}
+        if match:
+            req['parsed'] = dict()
+            for var_name in var_names:
+                req['parsed'][var_name] = match.group(var_name)
+        parsed_requirements.append(req)
+    return parsed_requirements
+
+
+def main(filename: Path, folder: Path):
+    out_file = folder / 'reqs.json'
+    reqs = parse_requirement_file(filename)
+
+    with open(out_file, 'w') as d_file:
+        json.dump(reqs, d_file)
+
+
+def main2(filename: Path, folder: Path):
+    out_file = folder / 'permitted.json'
+    reqs = parse_for_permitted_libs(filename)
+
+    with open(out_file, 'w') as d_file:
+        json.dump(reqs, d_file)
+
+
+if __name__ == '__main__':
+    output_folder = Path(__file__).parent.parent.parent.parent / 'output'
+    f = Path('/home/luiscberrocal/adelantos/adelantos-cupos/requirements/base.txt')
+    main(f, output_folder)
+
+    permitted_versions = output_folder / 'permitted_versions.json'
+    f = Path('/home/luiscberrocal/adelantos/ec-d-local-payment-collector/requirements/base.txt')
+    main2(f, output_folder)
