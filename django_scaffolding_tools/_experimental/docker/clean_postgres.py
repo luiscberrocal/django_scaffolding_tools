052c321bf2a01fe7f2d0993f1ad10f2e0a7a32d4
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2023-02-08 11:32:45 -0500

    Updated clean postgres.

--- django_scaffolding_tools/_experimental/docker/clean_postgres.py
+++ django_scaffolding_tools/_experimental/docker/clean_postgres.py
@@ -91,5 +91,5 @@ def bold_text(text: str, color: Optional[TerminalColor] = None):
 
 
 if __name__ == '__main__':
-    reg_expr = ProjectPostgresRegExp.PQP
+    reg_expr = ProjectPostgresRegExp.MPI
     do_cleanup(reg_expr)
