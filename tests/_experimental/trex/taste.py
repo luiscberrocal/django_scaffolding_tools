e876a5a87036088d1f72987d370e8b455f6e06f5
luiscberrocal <luis.berrocal.1942@gmail.com>
2023-05-10 18:45:36 -0500

    Regular expression

---
+++ tests/_experimental/trex/taste.py
@@ -0,0 +1,10 @@
+import trrex as tx
+import re
+
+def main():
+    pattern = tx.make(['122D', '125F'])
+    # mt = pattern.match('345D')
+    print(pattern)  # '\\b12(?:5F|2D)\\b'
+
+if __name__ == '__main__':
+    main()
